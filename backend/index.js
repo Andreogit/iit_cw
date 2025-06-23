2a8e678015ce0f6d963070f13283ca024903d2bc
Andreo <andrijsheremeta@ukr.net>
2025-06-23 14:22:30 +0300

    node fix

--- backend/index.js
+++ backend/index.js
@@ -198,6 +198,6 @@ app.post('/signUp', async (req, res) => {
   });
   
   // Start Server
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
   });
